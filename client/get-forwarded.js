7b4f40d76c32a244f7ffd16e199f887eb0701ee4
AJ ONeal <coolaj86@gmail.com>
2013-08-21 13:17:53 -0600

    secure connection between laptop and vps

--- client/get-forwarded.js
+++ client/get-forwarded.js
@@ -1,12 +1,23 @@
 (function () {
   'use strict';
 
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
   var ahr = require('ahr')
     , net = require('net')
+    , tls = require('tls')
+    , fs = require('fs')
     , config = require('./config')
     , remotePort
     , remoteUrl = 'http://' + config.remoteAddr + ':' + config.remoteWebPort
+    , tlsOptions
     ;
+    
+  tlsOptions = {
+    key: fs.readFileSync('new.pem')
+  , rejectUnauthorized: false
+  , ca: [ fs.readFileSync('server-cert.pem') ]
+  };
 
   ahr.http(remoteUrl, { username: config.username, password: config.password }).when(function (err, ahr, data) {
     var socket
@@ -15,8 +26,10 @@
 
     remotePort = data.port;
 
-    socket = net.connect(remotePort, config.remoteAddr);
-    socket.on('connect', function () {
+    socket = tls.connect(remotePort, config.remoteAddr);
+    socket.on('secureConnect', function () {
+    //socket.on('connect', function () {
+      console.log('remote secureConnect');
       socket.write('{ "master": true, "token": "' + data.token + '"}');
     });
     socket.on('data', function (chunk) {
@@ -36,8 +49,10 @@
         return;
       }
 
-      customer = net.connect(remotePort, config.remoteAddr);
-      customer.on('connect', function () {
+      customer = tls.connect(remotePort, config.remoteAddr);
+      customer.on('secureConnect', function () {
+        console.log('customer secureConnect');
+      //customer.on('connect', function () {
         customer.write('{ "token": "' + token + '" }');
         server = net.connect(config.localPort, 'localhost');
         customer.pipe(server);
