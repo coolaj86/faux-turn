1a12222bc1a7f2bfaafbae912aa9a2681e7f93ca
AJ ONeal <coolaj86@gmail.com>
2013-08-21 12:13:16 -0600

    typo fix

--- client/config.js
+++ client/config.js
@@ -6,8 +6,8 @@
   , password: 'bananas'
   , remoteAddr: 'example.com'
   , remoteWebPort: 8080
-  , localPort = 5050
-  , username = 'coolaj86'
-  , password = 'bananas'
+  , localPort: 5050
+  , username: 'coolaj86'
+  , password: 'bananas'
   };
 }());
